0a879061075424a214382ef3db57834912080dd4
MATHEUS DE ALMEIDA LUIZ <matheus.luiz7@senaisp.edu.br>
2026-05-29 16:11:20 -0300

    cors

--- E-COMMERCE/FRONT-END/src/pages/produtos/produtos.pages.js
+++ E-COMMERCE/FRONT-END/src/pages/produtos/produtos.pages.js
@@ -1,5 +1,4 @@
 import criarColuna from '../../components/shared/coluna-bootstrap.component';
-
 import criarCardProduto from '../../components/produtos/card.component';
 
 export async function personagensPage() {
@@ -16,44 +15,26 @@ export async function personagensPage() {
 
     const row = document.querySelector("#lista-produtos");
 
-    // PRODUTOS FIXOS
-   const produtos = [
+    try {
+        // Busca os dados diretamente do seu back-end na porta 8000
+        const response = await fetch("http://localhost:8000/produtos");
+        const produtos = await response.json();
 
-{
-    nome: 'Mouse gamer Infinity Ultra V8 Wireless',
-    marca: 'Infinity',
-    preco: '199,90',
-    imagem: '/Mouse-removebg-preview.png',
-    estoque: 12
-},
+        produtos.forEach(produto => {
 
-{
-    nome: 'Headset gamer Infinity Nebula H7 Wireless',
-    marca: 'Infinity',
-    preco: '349,90',
-    imagem: '/Fone-removebg-preview.png',
-    estoque: 7
-},
+            const coluna = criarColuna();
 
-{
-    nome: 'Gabinete Infinity Eclipse X1',
-    marca: 'Infinity',
-    preco: '599,90',
-    imagem: '/Gabinete-removebg-preview.png',
-    estoque: 4
-}
+            
+            produto.imagem = `http://localhost:8000/imagens/${produto.imagem}`;
 
-];
+            const card = criarCardProduto(produto);
 
-    produtos.forEach(produto => {
+            coluna.appendChild(card);
 
-        const coluna = criarColuna();
+            row.appendChild(coluna);
+        });
 
-        const card = criarCardProduto(produto);
-
-        coluna.appendChild(card);
-
-        row.appendChild(coluna);
-    });
-
+    } catch (error) {
+        console.error("Erro ao buscar produtos do back-end:", error);
+    }
 }
