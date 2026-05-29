a073924926697fdd91bf8ee2cb78d7bc568a129d
MIGUEL PEREIRA ARAUJO <miguel.araujo25@senaisp.edu.br>
2026-05-29 16:08:08 -0300

    CORS Atualização

--- E-COMMERCE/BACK-END/src/server.js
+++ E-COMMERCE/BACK-END/src/server.js
@@ -1,15 +1,17 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
-import cors from 'cors'
+import cors from 'cors';
+
+const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173'
 }));
-const app = express();
+app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use('/', routes);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Servidor rodando em http://localhost:${process.env.SERVER_PORT}`);
-})
+});
