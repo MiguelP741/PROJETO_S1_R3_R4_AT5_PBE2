5134fe3ec137047c7c26ca6a95f799b4a96c486d
MIGUEL PEREIRA ARAUJO <miguel.araujo25@senaisp.edu.br>
2026-05-29 15:21:18 -0300

    Produtos, Pedidos e ItensPedido BackEnd

---
+++ E-COMMERCE/BACK-END/src/repositories/produtoRepository.js
@@ -0,0 +1,29 @@
+import { connection } from "../configs/Database.js";
+
+const produtoRepository = {
+    criar: async (produto) => {
+        const sql = 'INSERT INTO Produtos (IdCategoria, NomeProduto, DescricaoProduto, Preco, Imagem, Estoque) VALUES (?, ?, ?, ?, ?, ?);';
+        const values = [produto.idCategoria, produto.nomeProduto, produto.descricaoProduto, produto.preco, produto.imagem, produto.estoque];
+        const [rows] = await connection.execute(sql, values);
+        return rows;
+    },
+    editar: async (produto) => {
+        const sql = 'UPDATE Produtos SET IdCategoria=?, NomeProduto=?, DescricaoProduto=?, Preco=?, Estoque=? WHERE IdProduto=?;';
+        const values = [produto.idCategoria, produto.nomeProduto, produto.descricaoProduto, produto.preco, produto.estoque, produto.idProduto];
+        const [rows] = await connection.execute(sql, values);
+        return rows;
+    },
+    deletar: async (idProduto) => {
+        const sql = 'DELETE FROM Produtos WHERE IdProduto = ?;';
+        const values = [idProduto];
+        const [rows] = await connection.execute(sql, values);
+        return rows;
+    },
+    selecionar: async () => {
+        const sql = 'SELECT * FROM Produtos;';
+        const [rows] = await connection.execute(sql);
+        return rows;
+    }
+}
+
+export default produtoRepository;
