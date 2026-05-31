fa4d5e8d3891b34482bab838ec839d5df9897d2e
Miguel araujo <miguelaraujo.fox05@gmail.com>
2026-05-31 19:04:16 -0300

    Correção de Organização

--- Back-End/src/middlewares/uploadImage.middleware.js
+++
@@ -1,9 +0,0 @@
-import createMulter from "../configs/upload.multer.js";
-
-const uploadImage = createMulter({
-    pasta: 'images',
-    tiposPermitidos: ['image/png', 'image/jpeg'],
-    tamanhoArquivo: 10 * 1024 * 1024 //10 MB
-}).single('image');
-
-export default uploadImage;
